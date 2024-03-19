febb7e9c3a7168fc98ff6993f8b9ae78c37d42f6
nagreme <nagreme@proton.me>
2024-03-19 08:43:33 -0400

    Initial commit

---
+++ digitizer_dict_snippet.py
@@ -0,0 +1,132 @@
+import sys
+import pdb
+
+filename = sys.argv[1]
+
+# column indices
+INDEX_CHANNEL_NUM = 0
+INDEX_LONG_GATE = 1
+INDEX_SHORT_GATE = 2
+INDEX_TIMESTAMP = 3
+INDEX_TIME = 4
+INDEX_DELTA_TIME = 5
+
+
+class DataPoint:
+    def __init__(self, channel_num, long_gate, short_gate, timestamp, time_ms, delta_time):
+        self.channel_num = channel_num
+        self.long_gate = long_gate
+        self.short_gate = short_gate
+        self.timestamp = timestamp
+        self.time = time_ms
+        self.delta_time = delta_time
+    
+    def __str__(self):
+        return f"DataPoint{self.__dict__}"
+    
+    def __repr__(self):
+        # return f"DataPoint{self.__dict__}"
+        return f"(chNum={self.channel_num}, time={self.timestamp})"
+    
+    def __eq__(self, other):
+        if isinstance(other, DataPoint):
+            return self.channel_num == other.channel_num
+        else:
+            return False
+    
+    def __hash__(self):
+        return hash(self.channel_num)
+
+
+timestamp_indexed_dict = {}
+timestamp_set = set()
+
+
+def build_dictionary(dead_time=0, gate_cutoff=2500, ratio_cutoff=0):
+    # this auto closes the file object at the end of the block
+    with open(filename) as file:
+        # read one file at a time
+        for line in file:
+            # extract data
+            columns = line.split()
+            channel_num = int(columns[INDEX_CHANNEL_NUM])
+            long_gate = int(columns[INDEX_LONG_GATE])
+            short_gate = int(columns[INDEX_SHORT_GATE])
+            timestamp = int(columns[INDEX_TIMESTAMP])
+            time = float(columns[INDEX_TIME])
+            delta_time = float(columns[INDEX_DELTA_TIME])
+            
+            # filtering
+            if delta_time < dead_time or long_gate < gate_cutoff or short_gate/long_gate < ratio_cutoff:
+                continue
+            
+            # build object
+            data_point = DataPoint(channel_num, long_gate, short_gate, timestamp, time, delta_time)
+            
+            # if we haven't seen this timestamp yet init with empty list
+            if timestamp not in timestamp_indexed_dict:
+                timestamp_indexed_dict[timestamp] = []
+            
+            # add your data point to the dictionary
+            timestamp_indexed_dict[timestamp].append(data_point)
+            
+            # append timestamp to masterlist
+            timestamp_set.add(timestamp)
+    
+    
+def main():
+    dead_times = [0]
+    gate_cutoff = 2500
+    ratio_cutoff = 0
+    coincidence_times = range(50, 150, 10)
+    
+    for dead_time in dead_times:
+        build_dictionary(dead_time)
+        
+        for ct in coincidence_times:
+            # bucket timestamps by coincidence_time
+            timestamp_groups = []
+            curr_group = []
+            timestamp_list = sorted(timestamp_set)
+            for timestamp in timestamp_list:
+                # starting a new group (first element)
+                if not curr_group: # is empty
+                    curr_group.append(timestamp)
+                # check if we need to append to the current group of if we create a new one
+                else:
+                    if timestamp - curr_group[0] < ct:
+                        curr_group.append(timestamp)
+                    else:
+                        timestamp_groups.append(curr_group)
+                        curr_group = [timestamp]
+                        
+            # rebuild a dict based on the new timestamp buckets
+            timestamp_group_dict = {}
+            
+            for group in timestamp_groups:
+                timestamp = group[0]
+                timestamp_group_dict[timestamp] = set()
+                for member in group:
+                    timestamp_group_dict[timestamp].update(timestamp_indexed_dict[member])
+                
+            # output result summary
+            counts = {
+                'single': 0,
+                'double': 0,
+                'double_12': 0,
+                'double_13': 0,
+                'double_23': 0,
+                'triple': 0
+            }
+            # TODO split double counts
+            for k, v in timestamp_group_dict.items():
+                coincidences = len(v)
+                counts[coincidences-1] += 1
+                # print(f'timestamp={k}: {len(v)}')
+            print(f"coincidence time={ct} => counts={counts}")
+            
+    
+    
+    
+if __name__ == '__main__':
+    main()
